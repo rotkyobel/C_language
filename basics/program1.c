4f8f6b2e602cb7d1a0b04e48f9dd7aa612337fb7
Yobel Tayson <rotkyobel@gmail.com>
2021-02-03 18:35:38 -0500

    Diferencias entre (void) y ()

---
+++ basics/program1.c
@@ -0,0 +1,7 @@
+// Program 1 (Compiles and runs fine in C, but not in C++)
+void fun() {}
+int main(void)
+{
+  fun(10, "GfG", "GQ");
+  return 0;
+}
---
+++ basics/program2.c
@@ -0,0 +1,7 @@
+// Program 2 (Fails in compilation in both C and C++)
+void fun(void) {}
+int main(void)
+{
+  fun(10, "GfG", "GQ");
+  return 0;
+}
