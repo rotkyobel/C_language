fcfec130674b5ec149e313ed018d7e95c1f8c5b3
Yobel Tayson <rotkyobel@gmail.com>
2021-02-03 18:18:01 -0500

    Hello World in C

--- age.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  int age = 0;
-  age = 37.2;
-  printf("%u", age);
-}
--- unsigned.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  unsigned char j = 255;
-  j = j + 10;
-  printf("%u", j); /* 9 */
-}
