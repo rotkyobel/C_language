e580fde15f5bbdc593a98d9da78e821c2e508373
Yobel Tayson <rotkyobel@gmail.com>
2021-02-06 22:08:16 -0500

    assignment operators

---
+++ Sctructured_Programming/assignment_operators.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+  printf("Operadores de Asignación");
+
+  int value;
+
+  value = 11;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  value += 1;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  value -= 2;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  value *= 5;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  value /= 4;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  value %= 6;
+
+  printf("El valor de la variable es: %i\n", value);
+
+  return 0;
+}
