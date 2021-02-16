ff174fa854af5b9cf956448b80cbbca94a44f6f3
Yobel Tayson <rotkyobel@gmail.com>
2021-02-16 12:25:14 -0500

    do while

---
+++ sctructured_programming/do_while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+  printf("Iterador Do While \n");
+
+  char answer = 'q';
+  char value;
+
+  do
+  {
+    printf("Ingresa una letra: ");
+    scanf(" %c", &value);
+
+  } while (value != answer);
+
+  printf("Coinciden las letras %c == %c", value, answer);
+
+  return 0;
+}
---
+++ sctructured_programming/do_while_practice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int num = 0;
+
+  do
+  {
+    printf("%i \n", num);
+    num++;
+  } while (num <= 100);
+
+  return 0;
+}
