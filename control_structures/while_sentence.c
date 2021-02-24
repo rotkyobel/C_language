b883f9e7b148efd9a304c38d332c8066b6c2a50d
Yobel Tayson <rotkyobel@gmail.com>
2021-02-24 14:21:01 -0500

    while sentence

---
+++ control_structures/while_sentence.c
@@ -0,0 +1,40 @@
+// TODO mostrar los 10 primeros numeros en pantalla - ascendente
+// TODO mostrar los 10 primeros numeros en pantalla - descendente
+// TODO mostrar 5 asteriscos
+
+#include <stdio.h>
+
+int main()
+{
+
+  int i;
+
+  /*
+  i = 1;
+
+  while (i <= 10)
+  {
+    printf("%i ", i); // 1 2 3 4 5 6 7 8 9 10
+    i++;
+  }
+  */
+  /*
+  i = 10;
+
+  while (i >= 0)
+  {
+    printf("%i ", i); // 10 9 8 7 6 5 4 3 2 1 0
+    i--;
+  }
+ */
+
+  i = 1;
+
+  while (i <= 5)
+  {
+    printf("*");
+    i++;
+  }
+
+  return 0;
+}
