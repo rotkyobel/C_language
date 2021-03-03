7118d6ca28213d281f0f95ffe34623b48c7cab12
Yobel Tayson <rotkyobel@gmail.com>
2021-03-02 21:26:33 -0500

    for sentence

---
+++ control_structures/for_sentence.c
@@ -0,0 +1,14 @@
+// Mostrar los 10 primeros numeros - ascendete - descendente
+
+#include <stdio.h>
+
+int main() {
+
+  int i;
+
+  for(i = 1; i <= 100; i++){
+    printf("%i. No debo llegar tarde a clases.\n", i);
+  }
+
+  return 0;
+}
